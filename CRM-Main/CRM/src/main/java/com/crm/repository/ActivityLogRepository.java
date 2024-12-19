1390aa2ef55bfa0847a0bffbdb60c5e880d69637
sonalimane1234 <77537520+sonalimane1234@users.noreply.github.com>
2024-12-19 17:05:24 +0530

    Delete CRM-Main/CRM directory

--- CRM-Main/CRM/src/main/java/com/crm/repository/ActivityLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crm.repository;
-
-import com.crm.entities.ActivityLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
-}
